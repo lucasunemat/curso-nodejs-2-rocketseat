240ec9896e26e6df445ebd0a2c9cbae304eb119c
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-25 14:40:39 -0400

    configurando local das migrations

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,18 @@
-import { knex as setupKnex } from 'knex' // importando o knex
+import { knex as setupKnex, Knex } from 'knex' // importando o knex
 
 // configurando o tipo de banco e dados de conexão (aqui iria user, senha, etc)
 // no caso de sqlite só precisa do filename
 
-export const config = {
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db', // caminho relativo ao arquivo que está guardando dados
+    filename: './db/app.db', // caminho relativo ao arquivo que está guardando dados
   },
   useNullAsDefault: true, // todos os campos sem info vão ser nulos por padrão
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 }
 
 export const knex = setupKnex(config)
