5729cae371624659940bac2dfc44077fd3441e8b
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-26 14:57:26 -0400

    sobre variaveis ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,9 @@
+import 'dotenv/config' // importando o dotenv para ler as variáveis de ambiente
 import { knex as setupKnex, Knex } from 'knex' // importando o knex e renomeando para setupKnex
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set')
+}
 
 // configurando o tipo de banco e dados de conexão (aqui iria user, senha, etc)
 // no caso de sqlite só precisa do filename
@@ -6,7 +11,7 @@ import { knex as setupKnex, Knex } from 'knex' // importando o knex e renomeando
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db', // caminho relativo ao arquivo que está guardando dados
+    filename: process.env.DATABASE_URL, // caminho relativo ao arquivo que está guardando dados
   },
   useNullAsDefault: true, // todos os campos sem info vão ser nulos por padrão
   migrations: {
